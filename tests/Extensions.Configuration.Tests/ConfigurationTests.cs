a5eafa52ea17dec623584da870002dcffdeb8d8e
ffernandolima <fernando.lima@warrenbrasil.com.br>
2020-06-25 22:00:57 -0300

    Added tests.

---
+++ tests/Extensions.Configuration.Tests/ConfigurationTests.cs
@@ -0,0 +1,16 @@
+using Xunit;
+
+namespace Extensions.Configuration.Tests
+{
+    public class ConfigurationTests
+    {
+        [Fact]
+        public void ConfigurationTest()
+        {
+            var config = ConfigurationFactory.Instance.GetConfiguration();
+            var value = config["ParentKey:ChildKey"];
+
+            Assert.Equal("xxx", value);
+        }
+    }
+}
