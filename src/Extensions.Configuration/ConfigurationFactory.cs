c1c5e8dbcef4fd10e6800fe1eca6c2473bf7e22b
ffernandolima <fernando.lima@warrenbrasil.com.br>
2020-06-25 21:05:33 -0300

    Initial Commit

---
+++ src/Extensions.Configuration/ConfigurationFactory.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace Extensions.Configuration
+{
+    public class ConfigurationFactory
+    {
+        private static readonly Lazy<ConfigurationFactory> Factory = new Lazy<ConfigurationFactory>(() => new ConfigurationFactory(), isThreadSafe: true);
+
+        public static ConfigurationFactory Instance => Factory.Value;
+
+        private IConfiguration _configuration;
+
+        private ConfigurationFactory() => _configuration = null;
+
+        private string EnvironmentVariable(string variable) => Environment.GetEnvironmentVariable(variable);
+
+        public IConfiguration GetConfiguration(bool optional = true, bool reloadOnChange = true)
+        {
+            if (_configuration == null)
+            {
+                IConfigurationBuilder builder = new ConfigurationBuilder()
+                    .Add(new XmlConfigurationProvider())
+                    .AddJsonFile("appsettings.json", optional, reloadOnChange)
+                    .AddJsonFile($"appsettings.{EnvironmentVariable("DOTNET_ENVIRONMENT")}.json", optional, reloadOnChange)
+                    .AddJsonFile($"appsettings.{EnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional, reloadOnChange)
+                    .AddEnvironmentVariables();
+
+                _configuration = builder.Build();
+            }
+
+            return _configuration;
+        }
+    }
+}
---
+++ src/Extensions.Configuration/XmlConfigurationProvider.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Configuration;
+
+namespace Extensions.Configuration
+{
+    public class XmlConfigurationProvider : ConfigurationProvider, IConfigurationSource
+    {
+        public IConfigurationProvider Build(IConfigurationBuilder builder) => this;
+
+        public override void Load()
+        {
+            foreach (ConnectionStringSettings connectionString in ConfigurationManager.ConnectionStrings)
+            {
+                Data.Add($"ConnectionStrings:{connectionString.Name}", connectionString.ConnectionString);
+            }
+
+            foreach (var settingKey in ConfigurationManager.AppSettings.AllKeys)
+            {
+                if (!Data.ContainsKey(settingKey))
+                {
+                    Data.Add(settingKey, ConfigurationManager.AppSettings[settingKey]);
+                }
+            }
+        }
+    }
+}
